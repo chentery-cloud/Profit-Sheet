c4d1d8357f7fc5c191a7def85d1a66118ec0d198
chentery-cloud <chentery@gmail.com>
2026-03-29 22:47:51 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
   /* ===============================
      ✅ API 및 환경 설정
   ================================ */
-  const API_URL = "https://script.google.com/macros/s/AKfycby9izGYO4BLI_SVIRHLfvobdJNel825i4VLHDQUw-Kxp5vL6PhCdH6HZPmkXTkBrlA_/exec";
+  const API_URL = "https://script.google.com/macros/s/AKfycbzI6mtM-kzrWSuG4dpIvFkeQeIXtwuvjF64bB8ItQHaREGCFWvxl8i2IBuh5B1zyGuw/exec";
   
   let isAdmin = false;
   const ADMIN_PASS = "admin1234";
